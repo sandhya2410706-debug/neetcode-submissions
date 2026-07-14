cb9db928ac6dc893451f7df72e8d7a451a6a31bf
Sandhya S <sandhya2410706@ssn.edu.in>
2026-07-14 17:43:58 +0530

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int):# -> int:
+       left=0
+       right=len(nums)
+
+       while(left<right):
+            mid=(left+right)//2
+            if(nums[mid]==target):
+                return mid
+            elif(nums[mid]<target):
+                left+=1
+            elif(nums[mid]>target):
+                right-=1
+            
+       return -1
